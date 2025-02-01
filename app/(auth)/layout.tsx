b9b48ac90eb59fc9b19a6f09aa6cc77cdc8802e0
Mohit1x <mohitvyas746@gmail.com>
2025-02-01 15:37:22 +0530

    initial folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout =({children}:{children:React.ReactNode})=>{
+    return(
+        <div className="bg-purple-300 h-full">
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-   <p className="text-xl hover:text-pink-500">Nexus Chat app</p>
-  );
-}
