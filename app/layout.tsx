a43c6a570ef8a26bf53c7182de0e9d676a718972
Mohit1x <mohitvyas746@gmail.com>
2025-04-10 12:13:49 +0530

    added auth and initial modal setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import localFont from "next/font/local";
+import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-[#313338]`)}
       >
+        <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem={false} storageKey="nexus-chat-theme">
         {children}
+        </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const InitialModal = () => {
+  return (
+    <div>
+      <h1>InitialModal</h1>
+    </div>
+  );
+};
+
+export default InitialModal;
